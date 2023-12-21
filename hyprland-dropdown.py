d1fff5b29f450ec9d844b7f5b53475ab05f7137b
hdev1 <husseinaljazairi@gmail.com>
2023-12-21 17:10:45 +0100

    fix: minor fixes

--- hyprland-dropdown.py
+++ hyprland-dropdown.py
@@ -55,7 +55,7 @@ config = json.load(open(config_path))
 
 # Sys util functions
 def exec_in_bg(command):
-    return subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+    return subprocess.Popen(command.split(" "))
 
 def exec(command):
     return os.system(command)
@@ -87,10 +87,10 @@ def get_clients():
 def get_active_client_state():
     return get_command_output_json("hyprctl activewindow -j")
 
-def get_client_state(id):
+def get_target_client_addresses(id):
     client_config = get_config_by_id(id)
     clients = get_clients()
-    matched_clients = [c for c in clients if c['class'] == client_config['class']]
+    matched_clients = [c['address'] for c in clients if c['class'] == client_config['class']]
 
     return matched_clients
 
@@ -99,16 +99,18 @@ def center_window(window):
     for i in range(0,2):
         exec(f'hyprctl --batch "dispatch focuswindow {window}; dispatch resizewindowpixel exact 75% 75%,{window}; dispatch centerwindow;"')
 
-def lock_window(active_client_state, no_lock=False):
+def tile_window(window, no_lock=False):
+    exec(f'hyprctl setprop {window} dimaround 0')
+    exec(f'hyprctl dispatch centerwindow {window}')
+    if not no_lock:
+        exec(f'hyprctl dispatch togglefloating {window}')
+
+def lock_client(active_client_state, no_lock=False):
     window = f"address:{active_client_state['address']}"
     print(window)
 
     if active_client_state['floating']:
-        exec(f'hyprctl setprop {window} dimaround 0')
-        exec(f'hyprctl dispatch centerwindow {window}')
-
-        if not no_lock:
-            exec(f'hyprctl dispatch togglefloating {window}')
+        tile_window(window, no_lock)
     else:
         exec(f'hyprctl setprop {window} dimaround 1')
         exec(f'hyprctl dispatch togglefloating {window}')
@@ -123,7 +125,7 @@ def lock_active(no_lock=False):
     if active_client_config == None:
         return
     
-    lock_window(active_client_state, no_lock=no_lock)
+    lock_client(active_client_state, no_lock=no_lock)
 
 def get_focused_monitor_and_workspace():
     monitors = get_command_output_json('hyprctl monitors -j')
@@ -132,33 +134,39 @@ def get_focused_monitor_and_workspace():
 
 def toggle(id, no_activate=False):
     client_config = get_config_by_id(id)
-    clients = get_client_state(id)
+    target_clients = get_target_client_addresses(id)
     focused_monitor_id, focused_workspace_id = get_focused_monitor_and_workspace()
     active_client_state = get_active_client_state()
+    all_clients = get_clients()
 
     # Not running? Launch new instance
-    if len(clients) == 0:
+    if len(target_clients) == 0:
         if no_activate:
             return
         
         exec_in_bg(client_config['launcher'])
         return
-    
+
     # Get all clients that match class; capture all of the subwindows
-    for client_state in clients:
+    for client_state in all_clients:
         window = f"address:{client_state['address']}"
+
+        if not client_state['address'] in target_clients:
+            if client_state['floating'] and get_config_by_class(client_state['class']) != None:
+                exec(f'hyprctl --batch "dispatch movetoworkspacesilent name:hidden,{window}; dispatch cyclenext; dispatch nodim"')
+            continue
         
         # Unlock and center/float window
-        lock_window(client_state, no_lock=True)
+        lock_client(client_state, no_lock=True)
 
         # Move window to current workspace
         if client_state['workspace']['id'] == -1337 or focused_monitor_id != client_state['monitor'] or focused_workspace_id != client_state['workspace']['id']:
             if no_activate:
-                return
+                continue
             
             exec(f'hyprctl --batch "dispatch movetoworkspacesilent e+0,{window};"')
             center_window(window)
-            return
+            continue
         
         if not no_activate and not active_client_state['address'] == client_state['address']:
             active_client_config = get_config_by_class(active_client_state['class'])
@@ -175,7 +183,7 @@ def reload():
 
     for window in config['windows']:
         rules.append(f"bind={window['keybind']},exec,{command} --toggle {window['id']}")
-        rules.append(f"windowrulev2 = nofullscreenrequest,class:{window['class']}")
+        # rules.append(f"windowrulev2 = nofullscreenrequest,class:{window['class']}")
         rules.append(f"windowrulev2 = float,class:{window['class']}")
         rules.append(f"windowrulev2 = size 75% 75%,class:{window['class']}")
         rules.append(f"windowrulev2 = move 30 50, class:{window['class']}")
